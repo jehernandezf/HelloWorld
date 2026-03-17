00ac06efff2d21950035f8b138aab4e19aa174c4
jehernandezf <javier.hf2795@gmail.com>
2026-03-16 20:14:45 -0600

    first commit

---
+++ src/mx/com/saludo/HelloWorld.java
@@ -0,0 +1,9 @@
+package mx.com.saludo;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World, this is simple java project to simulate a full CI/CD flow with Jenkins and Docker");
+	}
+
+}
