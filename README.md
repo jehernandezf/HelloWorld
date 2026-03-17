0dbeae743e6d9168d8643d46ace749faef5063f1
jehernandezf <javier.hf2795@gmail.com>
2026-03-16 22:47:04 -0600

    Add README

---
+++ README.md
@@ -0,0 +1,5 @@
+## This is simple java project to simulate a full CI/CD flow with Jenkins and Docker
+
+​<p align="left">
+    <img src="https://img.shields.io/badge/Java-%23ED8B00.svg?style=for-the-badge&logo=openjdk&logoColor=white" />
+</p>
--- src/mx/com/saludo/HelloWorld.java
+++ src/mx/com/saludo/HelloWorld.java
@@ -3,7 +3,7 @@ package mx.com.saludo;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World, this is simple java project to simulate a full CI/CD flow with Jenkins and Docker");
+		System.out.println("Hello World");
 	}
 
 }
